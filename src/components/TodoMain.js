b35c048444fdc48b7045876ee114e49e75f4b9e2
maejyomi <141015562+maejyomi@users.noreply.github.com>
2023-11-13 19:09:01 +0900

    추가 기능 구현
    - 엔터키로 insert
    - 내용 없을 때 insert (X)

--- src/components/TodoMain.js
+++ src/components/TodoMain.js
@@ -10,6 +10,10 @@ const TodoMain = () => {
 
     const addTodo = (e) => {
         console.log(content.current.value);
+        if(content.current.value == ""){
+            alert("할 일을 입력해주세요.");
+            return;
+        }
         
         const todo = {
             id: id,
@@ -40,6 +44,12 @@ const TodoMain = () => {
 
     }
 
+    const enterKeyDown = (e) => {
+        if(e.keyCode == 13){
+            addTodo();
+        }
+    }
+
     return (
         <div className="rounded-lg bg-white shadow-xl w-[512px] h-[650px] m-auto p-8">
             <div className="flex justify-between mb-6 text-xl">
@@ -47,7 +57,7 @@ const TodoMain = () => {
                 <p className="text-gray-400">{today[0]}.{today[1]}.{today[2]}</p>
             </div>
             <div className="flex items-center justify-center p-1 mb-8">
-                <input ref={content} type="text" name="contentText" placeholder="오늘의 할 일은?" 
+                <input ref={content} type="text" name="contentText" onKeyDown={enterKeyDown} placeholder="오늘의 할 일은?" 
                     className="rounded-lg border-solid border-gray-300 w-5/6 mr-2 focus:border-blue-300 focus:ring-blue-300" />
                 <button onClick={addTodo}>
                     <BiSolidMessageSquareAdd className="text-5xl text-blue-300 hover:text-blue-500" />
