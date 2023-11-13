31c61757fb98e782db6be110949c0cda630dddad
maejyomi <141015562+maejyomi@users.noreply.github.com>
2023-11-13 18:33:42 +0900

    insert 기능 구현

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,17 +1,67 @@
+import { useEffect, useState } from "react";
 import { FiEdit2 } from "react-icons/fi";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import { BsCheckLg } from "react-icons/bs";
 
 const TodoItem = ({ todo }) => {
-  console.log(todo);
+  const [check, setCheck] = useState(false);
+  const [edit, setEdit] = useState(false);
+  const handleCheck= (e) => {
+    setCheck(e.target.checked);
+  }
+
+  const handleEdit = () => {
+    console.log("클릭")
+    setEdit(true);
+  }
+
+  const submitEdit = () => {
+    console.log("편집 완료");
+    setEdit(false);
+  }
+
+  useEffect(()=>{
+    console.log(check);
+
+  },[check])
   return (
     <div className="flex justify-between my-2">
       <div className="flex items-center">
-        <input type="checkbox" className="border-gray-400 rounded-full mr-3" />
-        {todo.content}
+        
+        <input type="checkbox" onChange={handleCheck} className="border-gray-400 rounded-full mr-3" />
+        {
+          check
+          ? <p className="text-gray-300 line-through">{todo.content}</p>
+          : edit
+          ? 
+          <div>
+            <input type="text" defaultValue={todo.content} className="border-gray-400 rounded-lg p-1 pl-2"></input>
+          </div>
+          :
+          <div>
+            {todo.content}
+          </div>
+          
+        }
+        
       </div>
       <div className="flex items-center">
-        <button className="text-xl mr-2 text-gray-400 hover:text-green-700"><FiEdit2 /></button>
-        <button className="text-xl text-gray-400 hover:text-red-600"><RiDeleteBin6Line /></button>
+        {
+          check
+          ?<div>
+              <button onClick={handleEdit} disabled className="text-xl mr-2 text-gray-200"><FiEdit2 /></button>
+              <button className="text-xl text-gray-400 hover:text-red-600"><RiDeleteBin6Line /></button>
+            </div>
+          : edit
+          ?
+          <button onClick={submitEdit}><BsCheckLg  className="text-2xl text-gray-400 hover:text-green-500"/></button>
+          :
+            <div>
+              <button onClick={handleEdit} className="text-xl mr-2 text-gray-400 hover:text-blue-500"><FiEdit2 /></button>
+              <button className="text-xl text-gray-400 hover:text-red-600"><RiDeleteBin6Line /></button>
+            </div>
+        }
+        
       </div>
     </div>
   )
--- src/components/TodoMain.js
+++ src/components/TodoMain.js
@@ -1,23 +1,26 @@
+import { useState, useEffect, useRef } from "react";
 import TodoItemList from "./TodoItemList"
 import { BiSolidMessageSquareAdd } from "react-icons/bi";
 
 const TodoMain = () => {
     const today = new Date().toISOString().slice(0, 10).split("-");
+    const content = useRef();
+    const [id, setId] = useState(0);
+    const [todos, setTodos] = useState([]);
 
-    const todos = [
-        {
-            id: 1,
-            content: "insert"
-        },
-        {
-            id: 2,
-            content: "update"
-        },
-        {
-            id: 3,
-            content: "delete"
-        },
-    ]
+    const handleAdd = (e) => {
+        console.log(content.current.value);
+        
+        const todo = {
+            id: id,
+            content : content.current.value,
+        };
+
+        setTodos(todos.concat(todo));
+        setId(id+1);
+        content.current.value="";
+    }
+
     return (
         <div className="rounded-lg bg-white shadow-xl w-[512px] h-[650px] m-auto p-8">
             <div className="flex justify-between mb-6 text-xl">
@@ -25,9 +28,9 @@ const TodoMain = () => {
                 <p className="text-gray-400">{today[0]}.{today[1]}.{today[2]}</p>
             </div>
             <div className="flex items-center justify-center p-1 mb-8">
-                <input type="text" name="contentText" placeholder="오늘의 할 일은?" 
+                <input ref={content} type="text" name="contentText" placeholder="오늘의 할 일은?" 
                     className="rounded-lg border-solid border-gray-300 w-5/6 mr-2 focus:border-blue-300 focus:ring-blue-300" />
-                <button>
+                <button onClick={handleAdd}>
                     <BiSolidMessageSquareAdd className="text-5xl text-blue-300 hover:text-blue-500" />
                 </button>
             </div>
